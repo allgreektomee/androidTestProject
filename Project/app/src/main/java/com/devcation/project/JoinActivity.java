f1129c54635821638e7c211f9f8e38b23990c5ef
devcation <dynd0863@gmail.com>
2021-09-06 18:05:15 +0900

    join

--- Project/app/src/main/java/com/devcation/project/JoinActivity.java
+++ Project/app/src/main/java/com/devcation/project/JoinActivity.java
@@ -22,7 +22,7 @@ public class JoinActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.input_activity);
+        setContentView(R.layout.activity_join);
 
         edtPhone = findViewById(R.id.edtphone);
         edtName = findViewById(R.id.edtname);
